69b87fc192de12fc80ab8ecb2f656423ddae42f5
unknown <XGong2@bgc-xgong2.DIR.slb.com>
2014-11-10 16:52:31 +0800

    Before 2014-11-11, codes at Company PC

--- PopulatingNextRightPointer.java
+++ PopulatingNextRightPointer.java
@@ -1,35 +1,50 @@
-package com.programing;
-
+package com.test;
 
 class TreeLinkNode {
      int val;
      TreeLinkNode left, right, next;
-     TreeLinkNode(int x) { val = x; }
+     TreeLinkNode( int x) { val = x; }
  }
+
 public class PopulatingNextRightPointer {
-	public void connect(TreeLinkNode root) {
-		if (root == null)
-	      	return;
-	    if (root.right == null && root.left == null)
-	      	root.next = null;
-		TreeLinkNode node = root;
-		TreeLinkNode curr = root;
-		TreeLinkNode next = root;
-		while(next != null){
-			next = node.left;
-			while(node != null){
-				curr = node.left;
-				curr.next = node.right;
-				curr.next.next = node.next.left;
-				node = node.next;
-				
-			}
-			
-		}
-		
-		
-	      
-    }
-	  
-    
+	 public void connect(TreeLinkNode root) {
+         if (root == null)
+        	 return;
+        TreeLinkNode pre = root;
+        TreeLinkNode currHead = root;
+        root.next = null;
+        while(currHead.left!= null ){
+               currHead = pre. left;
+                while(pre != null ){
+                	
+                     pre.left.next = pre.right;
+                     if (pre.next == null)
+                    	 pre.right.next = null;
+                     else
+                    	 pre.right.next = pre.next.left;
+                     pre = pre.next;
+                     
+               }
+               pre = currHead;
+               
+        }
+        
+      
+}
+	 public static void main(String[] args){
+		 PopulatingNextRightPointer t = new PopulatingNextRightPointer();
+		   
+		   
+		 TreeLinkNode head = new TreeLinkNode(1);
+		 TreeLinkNode l = new TreeLinkNode(2);
+		 TreeLinkNode r = new TreeLinkNode(3);
+		 head.left = l;
+		 head.right = r;
+		 t.connect(head);
+		   
+		   
+		   
+		 System.out.println(head.left.next.val);
+
+	   }
 }
--- TwoSum.java
+++ TwoSum.java
@@ -1,34 +1,47 @@
-package com.programing;
+package com.test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TwoSum {
-    public int[] twoSum(int[] numbers, int target) {
-    	if(numbers == null || numbers.length == 0 || numbers.length == 1){
-    		return null;
-    	}
-    	int[] result = {-1,-1};
-    	
-        Map<Integer,Integer> hashMap = new HashMap<Integer, Integer>();
-       
-        for(int j = 0;j < numbers.length; j++){
-        	if(hashMap.containsKey(target - numbers[j])){
-
-				result[1] = j + 1;
-				result[0] = hashMap.get(target - numbers[j]) + 1;
-				break;
-        	}else{
-        		hashMap.put(numbers[j], j);
-        	}
-        }
-
-        return result;
-    }
-    
-    public static void main(String[] args){
-    	TwoSum t = new TwoSum();
-    	int[] numbers = {3,2,4};
-    	int target = 6;
-    	System.out.println(t.twoSum(numbers, target)[0]);
-    }
+	   public int[] twoSum(int[] numbers, int target) {
+		   int[] num = numbers.clone();
+		   Arrays.sort(num);
+		   
+		   int length = numbers.length;
+		   int left = 0;
+		   int right =length -1;
+		   int sum = 0;
+		   
+		   ArrayList<Integer> index = new ArrayList<Integer>();
+		   
+		   while (left < right){
+			   sum = num[left] + num[right];
+			   
+			   if (sum == target){
+				   for (int i = 0; i< length; ++i){
+					   if (numbers[i] == num[left]){
+						   index.add(i+1);
+					   }else if (numbers[i] == num[right]){
+						   index.add(i+1);
+					   }
+					   if (index.size() == 2){
+						   break;
+					   }
+				   }
+				   break;
+			   }else if(sum > target){
+				   --right;
+			   }else{
+				   ++left;
+			   }
+		   }
+		   
+		   int[] result = new int[2];
+		   result[0] = index.get(0);
+		   result[1] = index.get(1);
+		   
+		   return result;
+		   
+	   }
 }
